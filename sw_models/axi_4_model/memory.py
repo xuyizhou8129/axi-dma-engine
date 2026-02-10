d227d670d2772806d8e9c57285bd063c3ed8a9f7
Benjamin Gedeon Wong-Fodor <zqv1604@bane.ece.northwestern.edu>
2026-02-09 18:52:44 -0600

    system memory = an array

--- sw_models/axi_4_model/memory.py
+++ sw_models/axi_4_model/memory.py
@@ -1,9 +1,3 @@
 class SystemMemory:
     def __init__(self, size):
         self.mem = []*size # memory is just an array
-
-    def read(self, addr, size):
-        return self.mem[addr:addr+size] # indexes into array returns values
-
-    def write(self, addr, data):
-        self.mem[addr:addr+len(data)] = data # indexes into start address and writes data there
