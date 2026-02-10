d48bccaf9e3d64d220f23f121ba776f122eecaaa
Benjamin Gedeon Wong-Fodor <zqv1604@bane.ece.northwestern.edu>
2026-02-09 20:05:36 -0600

    axi_4 implenetation for reading/writng memory

--- sw_models/axi_4_model/axi_4_master.py
+++ sw_models/axi_4_model/axi_4_master.py
@@ -5,19 +5,22 @@ class AXI4Master:
     def __init__(self, memory: SystemMemory):
         self.memory = memory
         self.read_buffer = []
+        self.write_buffer = []
         
     def read_memory(self, start_addr, burst_length, datasize):
-        if start_addr < 0 or end_addr > self.memory.size:
-            raise ValueError("AXI read out of bounds")
-
         total_num_bytes = burst_length*datasize
         end_addr = start_addr + total_num_bytes
+
+        if start_addr < 0 or end_addr > self.memory.size:
+            raise ValueError("AXI read out of bounds")
 
         for i in range(start_addr, end_addr):
             self.read_buffer.append(self.memory.mem[i])
         
-    def read_a_byte(self, start_addr):
-        self.read_buffer.append(self.memory.mem[start_addr])
+    def read_a_byte(self, addr):
+        if addr < 0 or addr >= self.memory.size:
+            raise ValueError("AXI read out of bounds")
+        return self.memory.mem[addr]
     
     def write_memory(self, start_addr, burst_length, datasize, write_buffer):
         total_num_bytes = burst_length * datasize
@@ -31,4 +34,9 @@ class AXI4Master:
         for i in range(total_num_bytes):
             self.memory.mem[start_addr + i] = write_buffer[i]
 
+    def write_a_byte(self, start_addr, value):
+        if addr < 0 or addr >= self.memory.size:
+            raise ValueError("AXI write out of bounds")
+
+        self.memory.mem[addr] = value
 
