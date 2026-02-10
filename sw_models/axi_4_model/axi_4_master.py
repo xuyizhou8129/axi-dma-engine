a14841d08e30151b5c9308d3a4e1797ca15ec444
xuyizhou8129 <zhouxuyi20060401@gmail.com>
2026-02-09 19:36:06 -0600

    axi4 master

---
+++ sw_models/axi_4_model/axi_4_master.py
@@ -0,0 +1,9 @@
+class AXI4Master:
+    /*Key questions: How close are we to hardware?
+
+    def __init__(self, name, memory):
+        self.name = name
+        self.memory = memory
+
+    def read_memory(start_addr, )
+    def write()
