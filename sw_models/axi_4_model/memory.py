21daece815acad9ccea06a352414f50abfbe8a0f
Benjamin Gedeon Wong-Fodor <zqv1604@bane.ece.northwestern.edu>
2026-02-09 18:50:44 -0600

    commented code

--- sw_models/axi_4_model/memory.py
+++ sw_models/axi_4_model/memory.py
@@ -1,9 +1,9 @@
 class SystemMemory:
     def __init__(self, size):
-        self.mem = bytearray(size)
+        self.mem = []*size # memory is just an array
 
     def read(self, addr, size):
-        return self.mem[addr:addr+size]
+        return self.mem[addr:addr+size] # indexes into array returns values
 
     def write(self, addr, data):
-        self.mem[addr:addr+len(data)] = data
+        self.mem[addr:addr+len(data)] = data # indexes into start address and writes data there
