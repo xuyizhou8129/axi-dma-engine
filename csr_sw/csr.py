85dab2985b5ffcba8af0595930535572a120c23e
satviki <satviki.m@gmail.com>
2026-02-12 18:05:32 -0600

    started csr

---
+++ csr_sw/csr.py
@@ -0,0 +1,13 @@
+class CSR:
+
+    def __init__():
+        self.status
+
+    def set_status(new_status):
+
+        self.status = new_status
+
+    def get_status():
+        return self.status
+    
+    
