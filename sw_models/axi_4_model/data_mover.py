aa9e76d7e418b9dd5553c64a9351565938085a37
xuyizhou8129 <zhouxuyi20060401@gmail.com>
2026-02-12 19:27:58 -0600

    data_mover implementation

--- sw_models/axi_4_model/data_mover.py
+++ sw_models/axi_4_model/data_mover.py
@@ -1,7 +1,7 @@
 # Ben
-''' Description: The data mover itself contains a smaller data buffer
+"""Description: The data mover itself contains a smaller data buffer
 Functionalities:
-- Read data from AXI4 Master: 
+- Read data from AXI4 Master:
 input: start_addr, burst_length, data_size
 output: data (as an array)
 
@@ -16,21 +16,42 @@ output: flag (int for now)
 - Write data to SRAM Controller:
 input: start_addr, burst_length, data_size
 output: flag (int for now)
-'''
-import descriptor
+"""
+
+from axi_4_master import AXI4Master
+from sram_controller import SRAMController
+
 
 class DataMover:
-    def __init__(self, descriptor_fetcher, axi4_master, sram_controller, data_fifo)
-        self.descriptor_fetcher = descriptor_fetcher
-        self.axi4_master = axi4_master
-        self.sram_controller = sram_controller
-        self.data_fifo = data_fifo
-    
+    def __init__(
+        self,
+        axi4: AXI4Master,
+        sram: SRAMController,
+    ):
+        self.axi4 = axi4
+        self.sram = sram
+
     def read_from_axi4_master(self, start_addr, burst_length, data_size):
-        pass
+        # data_size is in bits; must be byte-multiple
+        if data_size % 8 != 0:
+            raise ValueError("data_size must be a multiple of 8 bits (1 byte)")
+
+        address = start_addr
+        for _ in range(burst_length):
+            self.axi.enqueue_read(address=address, size_bits=data_size)
+            address += data_size // 8  # advance by bytes per beat
+
+        return
 
     def read_from_sram_controller(self, start_addr, burst_length, data_size):
-        pass
+        # data_size is in bits; must be byte-multiple
+        if data_size % 8 != 0:
+            raise ValueError("data_size must be a multiple of 8 bits (1 byte)")
+
+        address = start_addr
+        for _ in range(burst_length):
+            self.sram.enqueue_read(address=address, size_bits=data_size)
+            address += data_size // 8  # advance by bytes per beat
 
     def write_to_axi4_master(self, start_addr, burst_length, data_size):
         pass
