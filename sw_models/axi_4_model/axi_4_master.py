0a3fdd1763a7b84b610339f3f2313e929b50e2bf
Benjamin Gedeon Wong-Fodor <zqv1604@moore.wot.ece.northwestern.edu>
2026-02-12 18:47:41 -0600

    updated axi4

--- sw_models/axi_4_model/axi_4_master.py
+++ sw_models/axi_4_model/axi_4_master.py
@@ -1,47 +1,54 @@
 import memory
 from memory import SystemMemory
+import fifo_queue
+from fifo_queue import FIFOQueue
 
 class AXI4Master:
     #/*Key questions: How close are we to hardware?*/
-    def __init__(self, memory: SystemMemory, ):
+    def __init__(self, memory: SystemMemory, df_read_fifo: FIFOQueue, df_write_fifo: FIFOQueue, dm_read_fifo: FIFOQueue, dm_write_fifo: FIFOQueue):
         self.memory = memory
+        self.df_read_fifo = df_read_fifo
+        self.df_write_fifo = df_write_fifo
+        self.dm_read_fifo = dm_read_fifo
+        self.dm_write_fifo = dm_write_fifo
 
     # add fifo data structure
     # fixed size read/write buffer
     # reset the buffers
         
-    def read_memory(self, start_addr, burst_length, datasize):
+    def read_memory(self, start_addr, burst_length, datasize, target_fifo: FIFOQueue):
         total_num_bytes = burst_length * datasize
         end_addr = start_addr + total_num_bytes
 
-        if start_addr < 0 or end_addr > self.memory.size: # out of bounds check
-            raise ValueError("AXI read out of b33unds")
-        
-        self.read_buffer.clear()   # clear FIRST
-        for i in range(start_addr, end_addr): # byte by byte we read form data 
-            self.read_buffer.append(self.memory.mem[i])
+        if start_addr < 0 or end_addr > self.memory.size:
+            raise ValueError("AXI read out of bounds")
+
+        for addr in range(start_addr, end_addr):
+            target_fifo.enqueue(self.memory.mem[addr])
   
-    def write_memory(self, start_addr, burst_length, datasize):
+  
+    def write_memory(self, start_addr, burst_length, datasize, source_fifo: FIFOQueue):
         total_num_bytes = burst_length * datasize
         end_addr = start_addr + total_num_bytes
 
-        if len(self.write_buffer) != total_num_bytes:
-            raise ValueError("Write buffer size mismatch")
         if start_addr < 0 or end_addr > self.memory.size:
             raise ValueError("AXI write out of bounds")
 
-        for i in range(total_num_bytes):
-            self.memory.mem[start_addr + i] = self.write_buffer[i]
-        self.write_buffer = []
+        if len(source_fifo) != total_num_bytes: # we wait for axi4 to write it into memory
+            raise ValueError("FIFO size mismatch for AXI write")
 
-    def write_a_byte(self, addr, value):
-        if addr < 0 or addr >= self.memory.size:
-            raise ValueError("AXI write out of bounds")
-        self.memory.mem[addr] = value
+        for addr in range(start_addr, end_addr):
+            self.memory.mem[addr] = source_fifo.dequeue()
+
+
+    # def write_a_byte(self, addr, value):
+    #     if addr < 0 or addr >= self.memory.size:
+    #         raise ValueError("AXI write out of bounds")
+    #     self.memory.mem[addr] = value
     
-    def read_a_byte(self, addr):
-        if addr < 0 or addr >= self.memory.size:
-            raise ValueError("AXI read out of bounds")
-        return self.memory.mem[addr]
+    # def read_a_byte(self, addr):
+    #     if addr < 0 or addr >= self.memory.size:
+    #         raise ValueError("AXI read out of bounds")
+    #     return self.memory.mem[addr]
 
 
