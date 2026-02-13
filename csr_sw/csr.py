7300b38a2514511f71abdc307ea8883dfdb3ef0e
axryap27 <aarya27@gmail.com>
2026-02-12 18:45:20 -0600

    control state register class added

--- csr_sw/csr.py
+++ csr_sw/csr.py
@@ -1,10 +1,17 @@
+import axi-lite
+from axi-lite import AxiLite
+
 class CSR:
+    def __init__(self, axi_lite: AxiLite):
+        #register representation via array
+        self.csr = [0]*32 
+        self.axi_lite = axi_lite
+        
 
-    def __init__():
-        self.status
 
+
+    def 
     def set_status(new_status):
-
         self.status = new_status
 
     def get_status():
