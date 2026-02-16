00afaa948526d3b2c9f8c696d5c112b647579407
b1982384 <benwong-fodor2028@u.northwestern.edu>
2026-02-16 17:56:19 -0600

    axi4 master implemented with one way fifo - need to udpated names still

--- sw_models/axi_4_model/axi_4_master.py
+++ sw_models/axi_4_model/axi_4_master.py
@@ -2,33 +2,57 @@ import memory
 from memory import SystemMemory
 import fifo_queue
 from fifo_queue import FIFOQueue
-
+import data_mover
+from data_mover import DataMover
+import descriptor_fetcher
+from descriptor_fetcher import DescriptorFetcher
 class AXI4Master:
     #/*Key questions: How close are we to hardware?*/
-    def __init__(self, memory: SystemMemory, df_read_fifo: FIFOQueue, df_write_fifo: FIFOQueue, dm_read_fifo: FIFOQueue, dm_write_fifo: FIFOQueue):
+    def __init__(self, memory: SystemMemory, fifo_to_df: FIFOQueue, fifo_to_dm: FIFOQueue, dm: DataMover, df: DescriptorFetcher):
         self.memory = memory
-        self.df_read_fifo = df_read_fifo
-        self.df_write_fifo = df_write_fifo
-        self.dm_read_fifo = dm_read_fifo
-        self.dm_write_fifo = dm_write_fifo
-        
-    def read_memory(self, start_addr, burst_length, datasize, target_fifo: FIFOQueue):
+        self.fifo_to_df = fifo_to_df
+        self.fifo_to_dm = fifo_to_dm
+        # self.dm = dm
+        # self.df = df
+        #self.fifo_from_dm = dm.data_fifo
+        self.fifo_from_df = df.df_write_fifo
+
+    def read_memory(self, start_addr, burst_length, datasize, target_fifo: FIFOQueue = None, target="dm"):
         total_num_bytes = burst_length * datasize
         end_addr = start_addr + total_num_bytes
 
         if start_addr < 0 or end_addr > self.memory.size:
             raise ValueError("AXI read out of bounds")
 
+        if target_fifo is None:
+            if target == "df":
+                target_fifo = self.fifo_to_df
+            elif target == "dm":
+                target_fifo = self.fifo_to_dm
+            else:
+                raise ValueError("Invalid AXI read target, use 'df' or 'dm'")
+
         for addr in range(start_addr, end_addr):
             target_fifo.enqueue(self.memory.mem[addr])
   
   
-    def write_memory(self, start_addr, burst_length, datasize, source_fifo: FIFOQueue):
+    def write_memory(self, start_addr, burst_length, datasize, source_fifo: FIFOQueue = None, source="dm"):
         total_num_bytes = burst_length * datasize
         end_addr = start_addr + total_num_bytes
 
         if start_addr < 0 or end_addr > self.memory.size:
             raise ValueError("AXI write out of bounds")
+
+        if source_fifo is None:
+            if source == "dm":
+                source_fifo = self.fifo_from_dm
+            elif source == "df":
+                source_fifo = self.fifo_from_df
+            else:
+                raise ValueError("Invalid AXI write source, use 'dm' or 'df'")
+
+        if source_fifo is None:
+            raise ValueError("AXI write source FIFO is not connected")
 
         if len(source_fifo) != total_num_bytes: # we wait for axi4 to write it into memory
             raise ValueError("FIFO size mismatch for AXI write")
@@ -46,5 +70,3 @@ class AXI4Master:
     #     if addr < 0 or addr >= self.memory.size:
     #         raise ValueError("AXI read out of bounds")
     #     return self.memory.mem[addr]
-
-
