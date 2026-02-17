8971a40175a9668f87edd9b640240c6d1139d618
xuyizhou8129 <zhouxuyi20060401@gmail.com>
2026-02-16 19:09:44 -0600

    data_mover structure done

--- sw_models/axi_4_model/data_mover.py
+++ sw_models/axi_4_model/data_mover.py
@@ -21,36 +21,59 @@ output: flag (int for now)
 from axi_4_master import AXI4Master
 from sram_controller import SRAMController
 from fifo_queue import FIFOQueue
+from descriptor import descriptor
 
+ADDRESSBITS = 32
+DATASIZEBITS = 3
+BUSRSTSIZEBITS = 16
 
 class DataMover:
     def __init__(
         self,
         axi4: AXI4Master,
         sram: SRAMController,
-        dm_write_fifo: FIFOQueue
+        dm_axi4_write_fifo: FIFOQueue
+        dm_sram_write_fifo: FIFOQueue
     ):
         self.axi4 = axi4
         self.sram = sram
-        self.dm_write_fifo = dm_write_fifo
+        self.dm_axi4_write_fifo = dm_axi4_write_fifo
+        self.dm_sram_write_fifo: FIFOQueue
 
     def read_from_axi4_master(self, start_addr, burst_length, data_size):
-        word_start_address = self.axi4.fifo_to_df.dequeue()
-        word_burst_length = self.axi4.fifo_to_df.dequeue()
-        word_datasize = self.axi4.fifo_to_df.dequeue()
+        data = self.axi4.fifo_to_dm.dequeue()  # gets a 32-bit data from axi4 master
+        return data
+        
+
 
     def read_from_sram_controller(self, start_addr, burst_length, data_size):
-        # data_size is in bits; must be byte-multiple
-        if data_size % 8 != 0:
-            raise ValueError("data_size must be a multiple of 8 bits (1 byte)")
-
-        address = start_addr
-        for _ in range(burst_length):
-            self.sram.enqueue_read(address=address, size_bits=data_size)
-            address += data_size // 8  # advance by bytes per beat
+        data = self.sram.fifo_to_dm.dequeue()  # gets a 32-bit data from sram controller
+        return data
 
     def write_to_axi4_master(self, start_addr, burst_length, data_size):
-        pass
+        raw_data = bin(self.dm_axi4_write_fifo.deque()) #Converts the value gotten in binary
+        address = raw_data[:ADDRESSBITS]
+        burst_size = raw_data[ADDRESSBITS:ADDRESSBITS+BURSTSIZEBITS]
+        data_width = raw_data[ADDRESSBITS+BURSTSIZEBITS : ADDRESSBITS+BURSTSIZEBITS+DATASIZEBITS]
+
+        new_descriptor = descriptor(
+            start_address = address,
+            burst_length = burst_size,
+            datasize = data_width
+        )
+
+        return new_descriptor
 
     def write_to_sram_controller(self, start_addr, burst_length, data_size):
-        pass
+        raw_data = bin(self.dm_sram_write_fifo.deque()) #Converts the value gotten in binary
+        address = raw_data[:ADDRESSBITS]
+        burst_size = raw_data[ADDRESSBITS:ADDRESSBITS+BURSTSIZEBITS]
+        data_width = raw_data[ADDRESSBITS+BURSTSIZEBITS : ADDRESSBITS+BURSTSIZEBITS+DATASIZEBITS]
+
+        new_descriptor = descriptor(
+            start_address = address,
+            burst_length = burst_size,
+            datasize = data_width
+        )
+
+        return new_descriptor
