0c7b63d42adc5fd1a3236b015945317202819fe3
aarya <aarya27@gmail.com>
2026-02-19 17:40:45 -0600

    IRQ fully implemented

--- csr_sw/irq.py
+++ csr_sw/irq.py
@@ -1,21 +1,23 @@
 class IRQ:
-    def __init__(self, CSR):
-        self.done = False
-        self.interrupt_enable = False
+    """
+    Software clears interrupts by writing 1 to IRQ_STATUS (W1C via CSR).
+    """
 
-    def set_interrupt(self):
-        self.interrupt_enable = True
+    def __init__(self, csr):
+        self.csr = csr
 
-    def interrupt(self):
-        self.interrupt_enable = False
-        CPU_DMA_Interrupt()
+    def irq_done(self):
+        irq_status = self.csr.read_register(self.csr.IRQ_STATUS_REG)
+        irq_enable = self.csr.read_register(self.csr.IRQ_ENABLE_REG)
+        done_set = (irq_status >> self.csr.IRQ_DONE_BIT) & 1
+        done_en  = (irq_enable >> self.csr.IRQ_DONE_BIT) & 1
+        return bool(done_set and done_en)
 
+    def irq_error(self):
+        irq_status = self.csr.read_register(self.csr.IRQ_STATUS_REG)
+        irq_enable = self.csr.read_register(self.csr.IRQ_ENABLE_REG)
+        return bool((irq_status >> self.csr.IRQ_ERROR_BIT) & 1) and \
+               bool((irq_enable >> self.csr.IRQ_ERROR_BIT) & 1)
 
-
-
-
-
-
-    
-    
-    
+    def any_pending(self):
+        return self.irq_done or self.irq_error
