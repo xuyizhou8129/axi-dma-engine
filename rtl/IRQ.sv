8f1f0b56b6192c42d1d65ee8f77fe90d0129e80a
aarya <aarya27@gmail.com>
2026-03-06 11:07:01 -0600

    removed logic out of ff, created comb signals

--- rtl/IRQ.sv
+++ rtl/IRQ.sv
@@ -6,8 +6,33 @@ module IRQ (
     input logic         error_event,    //pulse from data mover
     input logic         irq_en,         //CTRL from CSR
     input logic [1:0]   irq_clear,      //written by CPU
+    input logic         next_status_empty       //status flag for empty IRQ register
 
-    output logic [1:0]  irq_status,     //feeds back into IRQ_STATUS
-    output logic [1:0]  irq             //goes to CPU interrupt pin
+    output logic [1:0]  irq_status,     //feeds back into IRQ_STATUS in CSR? (unsure)
+    output logic        irq             //goes to CPU interrupt pin
 );
+    // comb signals
+    logic [1:0] irq_status_c;
+    //flip flop for empty_event
+    logic status_empty;
+    logic status_error;
+
+    always_comb begin 
+        next_status_empty = status_empty;
+
+        if (empty_event)
+            next_status_empty = 1;
+        else if (irq_clear[0]) 
+            next_status_empty = 0;
+    end
+
+    // latches the value on rising edge
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            status_empty <= 0;
+        else
+            status_empty <= next_status_empty;
+        end
+
 endmodule
+
