699f30d2f6b4d5c5a9c4b31be0bd4f41d925aa3f
aarya <aarya27@gmail.com>
2026-04-10 14:48:46 -0500

    comment changes

--- rtl/IRQ.sv
+++ rtl/IRQ.sv
@@ -3,7 +3,7 @@ module IRQ (
     input logic         rst_n,
 
     input logic         empty_event,    //pulse from ring manager
-    input logic         error_event,    //pulse from data mover/ring manager
+    input logic         error_event,    //pulse from ring manager
     input logic         irq_en,         //CTRL from CSR
     input logic [1:0]   irq_clear,      //written by CPU
     input logic         global_en,
