7aea23958ac6d208008422df188e824e4a8e8d19
Xuyi Zhou <zhouxuyi20060401@gmail.com>
2026-05-01 13:24:27 -0500

    let leave resolve which side of the port it is

--- rtl/vivado_config.sv
+++ rtl/vivado_config.sv
@@ -82,7 +82,7 @@ module vivado_config #(
     ) u_dma_top (
         .clk              (clk),
         .rst_n            (rst_n),
-        .soc_bus          (soc_bus.soc),
+        .soc_bus          (soc_bus),
         .axi_sys          (axi_sys.master),
         .irq_rm_empty     (irq_rm_empty),
         .irq_rm_error     (irq_rm_error),
